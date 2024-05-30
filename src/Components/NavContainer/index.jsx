b337dfbc58565009846217f40eca253c2df6792d
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-05-30 11:31:53 +0545

    removed extra code form home page (#10)
    Co-authored-by: user <user@usernoMacBook-Air.local>

--- src/Components/NavContainer/index.jsx
+++ src/Components/NavContainer/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../HorizontalNavBar'
 import Nav from '../Nav'
 import Paper from '../Template/Paper'
@@ -6,21 +6,26 @@ import Paper2 from '../Template2/Paper2'
 import Paper3 from '../Template3/Paper3'
 
 const NavContainer = ({ children }) => {
-  const [slideCount, setSliderCount] = useState(1)
+  const [cvTemplate, setCvTemplate] = useState(1)
 
-  const handelTemplateButton = (page) => {
-    setSliderCount(page)
-    localStorage.setItem('slideCount', page)
+  useEffect(() => {
+    const cvTemplateNumber = localStorage.getItem('cvTemplate') * 1
+    setCvTemplate(cvTemplateNumber)
+  }, [cvTemplate])
+
+  const handelTemplateButton = (templateNumber) => {
+    localStorage.setItem('cvTemplate', JSON.stringify(templateNumber))
+    setCvTemplate(templateNumber)
   }
 
   const renderCvFormat = (id) => {
-    if (id === 1) {
-      return <Paper />
+    if (id === 3) {
+      return <Paper3 />
     }
     if (id === 2) {
       return <Paper2 />
     }
-    return <Paper3 />
+    return <Paper />
   }
   return (
     <>
@@ -32,8 +37,8 @@ const NavContainer = ({ children }) => {
           <div className="flex justify-between bg-gray-700 p-2 text-xl px-4">
             <p
               onClick={() => handelTemplateButton(1)}
-              className={`  w-auto cursor-pointer ${
-                slideCount === 1 ? 'text-white' : 'text-gray-200'
+              className={`w-auto cursor-pointer ${
+                cvTemplate === 1 ? 'text-white' : 'text-gray-400'
               }`}
             >
               Template 1
@@ -41,7 +46,7 @@ const NavContainer = ({ children }) => {
             <p
               onClick={() => handelTemplateButton(2)}
               className={`  w-auto cursor-pointer ${
-                slideCount === 2 ? 'text-white' : 'text-gray-200'
+                cvTemplate === 2 ? 'text-white' : 'text-gray-400'
               }`}
             >
               Template 2
@@ -49,13 +54,13 @@ const NavContainer = ({ children }) => {
             <p
               onClick={() => handelTemplateButton(3)}
               className={`  w-auto cursor-pointer ${
-                slideCount === 3 ? 'text-white' : 'text-gray-200'
+                cvTemplate === 3 ? 'text-white' : 'text-gray-400'
               }`}
             >
               Template 3
             </p>
           </div>
-          {renderCvFormat(slideCount)}
+          {renderCvFormat(cvTemplate)}
         </div>
       </div>
     </>
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from 'react'
 import { templates } from '../utils/Data'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import { Link, useNavigate } from 'react-router-dom'
 
 function Slider() {
-  const navigate = useNavigate()
-  const [isLogin, setIsLogin] = useState('')
+  const navigator = useNavigate()
+
+  const handelClick = (id) => {
+    localStorage.setItem('cvTemplate', JSON.stringify(id))
+    navigator('/basicInfo')
+  }
 
   const slideLeft = () => {
-    var slider = document.getElementById('slider')
+    let slider = document.getElementById('slider')
     slider.scrollLeft = slider.scrollLeft - 400
   }
 
   const slideRight = () => {
-    var slider = document.getElementById('slider')
+    let slider = document.getElementById('slider')
     slider.scrollLeft = slider.scrollLeft + 400
   }
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('userId'))
-    if (data) {
-      setIsLogin(data)
-    }
-  }, [])
 
   return (
     <>
@@ -35,20 +31,21 @@ function Slider() {
         />
         <div
           id="slider"
-          className="w-full h-full overflow-x-hidden overflow-y-hidden whitespace-nowrap scroll scroll-smooth  "
+          className="w-full h-full overflow-hidden whitespace-nowrap scroll scroll-smooth"
         >
-          {templates.map((item) => (
-            <div key={item.id} className="inline-block relative mb-7 group">
+          {templates.map(({ id, img }) => (
+            <div key={id} className="inline-block relative mb-7 group">
               <img
-                className={`w-[480px]  p-2 mx-7 group-hover:scale-105 ease-in-out duration-300`}
-                src={item.img}
+                className={`w-[480px] p-2 mx-7 group-hover:scale-105 ease-in-out duration-300`}
+                src={img}
                 alt="/"
               />
-              <Link to={'/basicInfo'}>
-                <button className="p-3 rounded-lg bg-yellow-300 text-xl absolute bottom-5 group-hover:block hidden left-1/3">
-                  Use Template
-                </button>
-              </Link>
+              <button
+                onClick={() => handelClick(id)}
+                className="p-3 rounded-lg bg-yellow-300 text-xl absolute bottom-5 group-hover:block hidden left-1/3"
+              >
+                Use Template
+              </button>
             </div>
           ))}
         </div>
