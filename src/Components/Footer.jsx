b9653d738a4dbe251f7273d233c85d2574ee0469
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-05-31 08:52:46 +0545

    updated footer component (#19)
    Co-authored-by: user <user@usernoMacBook-Air.local>

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,33 +1,31 @@
-import React from "react"
+import React from 'react'
 //
-import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import { FaFacebook, FaGithub, FaInstagram } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import CvContainer from './CvContainer'
 
 const Footer = () => {
   return (
     <div className="bg-[#06536f] py-6 text-[#DDE8F0]">
-      <div className="flex justify-around items-start capitalize text-lg border-b-2 border-gray-300 py-5 px-6">
-        <div className="flex-1 text-center">logo</div>
-        <div className="flex items-start justify-between flex-1">
+      <CvContainer>
+        <div className="flex justify-around capitalize text-lg py-5 px-6">
+          <div className="text-xl font-semibold pb-1">logo</div>
           <div>
-          <h2 className="text-xl font-semibold pb-1">Service</h2>
+            <h2 className="text-xl font-semibold pb-1">Service</h2>
             <ul className="text-gray-200">
               <li className="cursor-pointer hover:text-white duration-200">
-                <Link to={"/template"}>create CV</Link>
+                <Link to={'/basicInfo'}>create CV</Link>
               </li>
               <li className="hover:text-white duration-200 cursor-pointer">
-                <Link to={"/template"}>CV Template</Link>
+                About Us
               </li>
               <li className="hover:text-white duration-200 cursor-pointer">
-                <Link to={"/aboutus"}>About Us</Link>
-              </li>
-              <li className="hover:text-white duration-200 cursor-pointer">
-                <Link to={"/login"}>Login</Link>
+                <Link to={'/login'}>Login</Link>
               </li>
             </ul>
           </div>
           <div>
-          <h2 className="text-xl font-semibold pb-1">Legal Terms</h2>
+            <h2 className="text-xl font-semibold pb-1">Legal Terms</h2>
             <ul className="text-gray-200">
               <li className="cursor-pointer hover:text-white duration-200">
                 <Link to="">Privacy Policy</Link>
@@ -35,7 +33,6 @@ const Footer = () => {
               <li className="hover:text-white duration-200 cursor-pointer">
                 <Link to="">Use of Terms</Link>
               </li>
-              
             </ul>
           </div>
           <div>
@@ -47,18 +44,20 @@ const Footer = () => {
               <li className="hover:text-white duration-200 cursor-pointer">
                 <Link to="">Contact Us</Link>
               </li>
-              
             </ul>
           </div>
         </div>
-      </div>
-      <div className="pt-4">
+      </CvContainer>
+
+      {/* copyright section */}
+      <div className="pt-4 border-t-2 border-gray-300">
         <div className="flex justify-center gap-4 mb-2">
           <FaFacebook className="text-3xl cursor-pointer hover:scale-110 duration-300 rounded-full border border-gray-400" />
           <a
-            href="https://github.com/Shivakc7/College-Project"
+            href="https://github.com/subashprj55/cvMaker"
             rel="noreferrer"
-            target="_blank">
+            target="_blank"
+          >
             <FaGithub className="text-3xl cursor-pointer hover:scale-110 duration-300 rounded-full border border-gray-400" />
           </a>
           <FaInstagram className="text-3xl cursor-pointer hover:scale-110 duration-300 " />
