7e74d53d022a1c65fbb56d3ae0ac7d6c43251f39
subashprj55 <99418785+subashprj55@users.noreply.github.com>
2024-05-31 22:11:38 +0545

    nav bar updated (#21)
    Co-authored-by: user <user@usernoMacBook-Air.local>

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,30 +1,81 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import CvContainer from './CvContainer'
+import { FaBars } from 'react-icons/fa'
+import { IoCloseSharp } from 'react-icons/io5'
+import { useState } from 'react'
+import { SideBarData } from './HorizontalNavBar/data'
 
 const Nav = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
+  const handleNavButton = () => {
+    setIsNavOpen(!isNavOpen)
+  }
+
   return (
     <>
-      <nav className="w-full bg-gray-600 text-[#DDE8F0] ">
+      <nav
+        className={`${
+          isNavOpen ? 'fixed' : ''
+        } w-full bg-gray-600 text-[#DDE8F0]`}
+      >
         <CvContainer>
           <div className="flex justify-between items-center py-2">
             <h2 className="text-2xl md:text-3xl">
               <Link to="/">Smart CV</Link>
             </h2>
-            <ul className="w-auto flex justify-between gap-4 md:gap-10 cursor-pointer md:text-xl">
-              <>
-                <li>
-                  <Link to="/login"> Login</Link>
-                </li>
-                <li>
-                  <Link to="/signUp"> Register</Link>
-                </li>
-              </>
+            <ul className="w-auto hidden md:flex justify-between gap-4 md:gap-10 cursor-pointer md:text-xl">
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signUp">Register</Link>
+              </li>
             </ul>
+            <div className="md:hidden">
+              {isNavOpen ? (
+                <IoCloseSharp className="text-2xl" onClick={handleNavButton} />
+              ) : (
+                <FaBars className="text-xl" onClick={handleNavButton} />
+              )}
+            </div>
           </div>
         </CvContainer>
       </nav>
+      <SideNavBar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
     </>
   )
 }
 
 export default Nav
+
+const SideNavBar = ({ isNavOpen, setIsNavOpen }) => {
+  const navigate = useNavigate()
+  const pathName = window.location.pathname
+
+  const handleClick = (link) => {
+    setIsNavOpen(false)
+    navigate(link)
+  }
+
+  return (
+    <div
+      className={`lg:hidden fixed top-12 left-0 w-screen h-screen bg-white z-50 transition-transform duration-200 ${
+        isNavOpen ? 'translate-x-0' : 'translate-x-full'
+      }`}
+    >
+      {SideBarData.map(({ id, name, icon: Icon, link }) => (
+        <div
+          key={id}
+          onClick={() => handleClick(link)}
+          className={`${
+            pathName === link ? 'bg-blue-500 text-white' : 'hover:bg-gray-200'
+          } capitalize cursor-pointer mb-2 p-3 transition-colors duration-300 flex items-center gap-3`}
+        >
+          <Icon className="text-lg" />
+          <h1 className="capitalize">{name}</h1>
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/pages/PreViewPdf.jsx
+++ src/pages/PreViewPdf.jsx
@@ -12,13 +12,47 @@ const PreViewPdf = () => {
     setCvTemplate(cvTemplateNumber)
   }, [cvTemplate])
 
+  const handelTemplateButton = (templateNumber) => {
+    console.log(templateNumber)
+    localStorage.setItem('cvTemplate', JSON.stringify(templateNumber))
+    setCvTemplate(templateNumber)
+  }
+
   return (
     <>
-      <Preview id={'jsx-template'}>
-        {cvTemplate === 1 ? <Paper /> : ''}
-        {cvTemplate === 2 ? <Paper2 /> : ''}
-        {cvTemplate === 3 ? <Paper3 /> : ''}
-      </Preview>
+      <div className="w-screen mt-20">
+        <Preview id={'jsx-template'}>
+          {cvTemplate === 1 ? <Paper /> : ''}
+          {cvTemplate === 2 ? <Paper2 /> : ''}
+          {cvTemplate === 3 ? <Paper3 /> : ''}
+        </Preview>
+      </div>
+      <div className="flex lg:hidden justify-between bg-gray-700 p-2 text-xl px-4">
+        <p
+          onClick={() => handelTemplateButton(1)}
+          className={`w-auto cursor-pointer ${
+            cvTemplate === 1 ? 'text-white' : 'text-gray-400'
+          }`}
+        >
+          Template 1
+        </p>
+        <p
+          onClick={() => handelTemplateButton(2)}
+          className={`  w-auto cursor-pointer ${
+            cvTemplate === 2 ? 'text-white' : 'text-gray-400'
+          }`}
+        >
+          Template 2
+        </p>
+        <p
+          onClick={() => handelTemplateButton(3)}
+          className={`  w-auto cursor-pointer ${
+            cvTemplate === 3 ? 'text-white' : 'text-gray-400'
+          }`}
+        >
+          Template 3
+        </p>
+      </div>
       <div className="relative mt-10 mb-36">
         <button
           className="absolute right-20 capitalize text-lg rounded-xl py-2 px-6 bg-sky-600 text-white hover:bg-sky-700 "
